30c50c80b519f858d845e0efe5b7a4b26a7ba247
Juan Pablo Gonzalez <juanpigonzalez72@gmail.com>
2023-11-22 17:53:14 -0300

    Se cambia el nombre a la solucion

--- FrmView/FrmView/obj/Debug/net6.0-windows/FrmView.AssemblyInfo.cs
+++ FrmView/FrmView/obj/Debug/net6.0-windows/FrmView.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrmView")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11558476d0fcb856d1f37eeb92116da5a6a80aec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrmView")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FrmView")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
